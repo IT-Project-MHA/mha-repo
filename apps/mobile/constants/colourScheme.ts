a047dd643343f58643c1a6cc802272dc6c642e8f
Amelia Coccia <ameliarc@ifi.no>
2026-09-15 21:36:28 +1000

    theme template reconfigured and updated

--- apps/mobile/constants/colourScheme.ts
+++ apps/mobile/constants/colourScheme.ts
@@ -1,51 +1,89 @@
 /**
  * Colour schemes across themes light / dark / light HC / dark HC
+ * Themes can easily be extended, either by matching the 
+ * ColourSet type, or by adjusting it.
  */
 
-// make shape of theme explicit 
+
+// standard theme layout
 export type ColourSet = {
-  p1: string;
-  p2: string;
-  p3: string;
-  p4: string;
-  p5: string;
+  background: string;
+  surface: string;
+  primary: string;
+  secondary: string;
+  tertiary: string;
+  onBackground: string;
+  onSurface: string;
+  onPrimary: string;
+  onSecondary: string;
+  onTertiary: string;
+
 };
 
-// M3 / sys / light
+/**
+ * Light mode
+ * M3 / sys / light in figma
+ */
 export const lightColours: ColourSet = {
-  p1: '#4F378B',
-  p2: '#F7F2FA',
-  p3: '#6750A4',
-  p4: '#000000',
-  p5: '#EADDFF',
+  background: '#FFFFFF',
+  surface: '#F2F2F7',
+  primary: '#6750A4',
+  secondary: '#e8def8',
+  tertiary: '#4a4459',
+  onBackground: '#000000',
+  onSurface: '#1D1B20',
+  onPrimary: '#FFFFFF',
+  onSecondary: '#6750A4',
+  onTertiary: '#FEF7FF',
 };
 
-// M3 / sys / dark
+/**
+ * Dark mode
+ * M3 / sys / dark in figma
+ */
+ 
 export const darkColours: ColourSet = {
-  p1: 'pink',
-  p2: '#F7F2FA',
-  p3: '#6750A4',
-  p4: '#000000',
-  p5: '#EADDFF',
+  background: '#1D1B20',
+  surface: '#4a4459',
+  primary: '#6750A4',
+  secondary: '#e8def8',
+  tertiary: '#4a4459',
+  onBackground: '#FFFFFF',
+  onSurface: '#CED0D4',
+  onPrimary: '#FFFFFF',
+  onSecondary: '#6750A4',
+  onTertiary: '#FEF7FF',
 };
-
 
 // M3 / sys / light / high contrast 
 export const lightHcColours: ColourSet = {
-  p1: 'blue',
-  p2: '#F7F2FA',
-  p3: '#6750A4',
-  p4: '#000000', 
-  p5: '#EADDFF',
+  background: '#FFFFFF',
+  surface: '#F2F2F7',
+  primary: '#6750A4',
+  secondary: '#e8def8',
+  tertiary: '#4a4459',
+  onBackground: '#000000',
+  onSurface: '#1D1B20',
+  onPrimary: '#FFFFFF',
+  onSecondary: '#6750A4',
+  onTertiary: '#FEF7FF'
 };
-
 
 // M3 / sys / dark / high contrast
 export const darkHcColours: ColourSet = {
-  p1: 'red',
-  p2: '#F7F2FA',
-  p3: '#6750A4',
-  p4: '#000000',
-  p5: '#EADDFF',
+  background: '#FFFFFF',
+  surface: '#F2F2F7',
+  primary: '#6750A4',
+  secondary: '#e8def8',
+  tertiary: '#4a4459',
+  onBackground: '#000000',
+  onSurface: '#1D1B20',
+  onPrimary: '#FFFFFF',
+  onSecondary: '#6750A4',
+  onTertiary: '#FEF7FF'
 };
 
+/**
+ * Extra colours for graphics, etc.
+ * Add colours here as you use them.
+ */
