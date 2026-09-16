17aef4ec84bbd19e54d3f7fab652915ffed8d0ed
Amelia Coccia <ameliarc@ifi.no>
2026-09-16 12:47:47 +1000

    added ButtonType variable- type now declared on component creation

--- apps/mobile/components/Button.tsx
+++ apps/mobile/components/Button.tsx
@@ -3,20 +3,23 @@ import React from 'react';
 import { StyleSheet, Text, TouchableOpacity } from 'react-native';
 import { useTheme } from '../context/ThemeContext';
 
+type ButtonType = "primaryButton" | "secondaryButton" ;
+
 //defines the shape
 type ButtonProps = {
   label: string;
   onPress: () => void;
+  buttonType: ButtonType;
 };
 
 //uses shape to check passed in props
-const Button = ({ label, onPress}: ButtonProps) => {
+const PrimaryButton = ({label, onPress, buttonType}: ButtonProps) => {
   const { theme } = useTheme();
-  // const {buttonType} = 
+
   return (
     <TouchableOpacity
       activeOpacity={0.7}
-      style={theme.primaryButton} // should be theme.buttonType
+      style={theme[buttonType]} // should be button stype
       onPress={onPress}
     >
       <Text style={theme.text}>{label}</Text>
@@ -24,4 +27,6 @@ const Button = ({ label, onPress}: ButtonProps) => {
   );
 };
 
-export default Button;
+
+export default PrimaryButton;
+
--- apps/mobile/constants/layout.ts
+++ apps/mobile/constants/layout.ts
@@ -1,15 +1,25 @@
 /**
- * Layout constants for components, retreived from client Figma
+ * StyleSheet for components.
  */
 
+// basic button
 export const buttonLayout = {
-  paddingVertical: 12, //height
-  paddingHorizontal: 55, //width
+  paddingVertical: 12, 
+  paddingHorizontal: 55, 
   borderRadius: 8,
   alignItems: 'center' as const,
 };
 
+// text– type 1
 export const textLayout = {
   fontSize: 16,
   fontWeight: 'bold' as const,
+};
+
+// primary button
+export const primaryButtonLayout = {
+  paddingVertical: 12, 
+  paddingHorizontal: 80, 
+  borderRadius: 8,
+  alignItems: 'center' as const,
 };
