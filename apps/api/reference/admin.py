d6f277c216abda9b142ecab9c7abb285d32c95cb
jhunter7333 <jhunter7333@gmail.com>
2026-09-17 10:02:33 +1000

    Reference and health registered

--- apps/api/reference/admin.py
+++ apps/api/reference/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import PainType, PainCharacteristic, PainLocation
 # Register your models here.
+
+admin.site.register(PainLocation)
+admin.site.register(PainCharacteristic)
+admin.site.register(PainType)
