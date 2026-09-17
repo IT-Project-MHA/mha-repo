deed07f70abfbb0286b6fc9c306f977c038369d4
victorialiu2 <victoria.liu2@student.unimelb.edu.au>
2026-09-17 10:03:01 +1000

    registered models in apps/accounts, appointment, audit
    SCRUM-94

--- apps/api/accounts/admin.py
+++ apps/api/accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(UserManager)
+admin.site.register(User)
+admin.site.register(PatientProfile)
+admin.site.register(UserSettings)
+admin.site.register(SupportLink)
+admin.site.register(TermsAndPrivacy)
--- apps/api/appointment/admin.py
+++ apps/api/appointment/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(Appointment)
+admin.site.register(CarePerson)
+admin.site.register(AppointmentQuestion)
+admin.site.register(AppointmentAnswer)
+admin.site.register(AppointmentAccess)
+admin.site.register(AppointmentAccess)
+admin.site.register(AppointmentAccess)
+admin.site.register(AppointmentAccess)
+admin.site.register(AppointmentAccess)
+admin.site.register(AppointmentAccess)
