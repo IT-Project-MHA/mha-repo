23ea97fe173abf10f48ae05a036b29839bf28c3e
jhunter7333 <jhunter7333@gmail.com>
2026-09-17 10:44:46 +1000

    Fixed user manager admin problem

--- apps/api/accounts/admin.py
+++ apps/api/accounts/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import UserManager, User, PatientProfile, UserSettings, SupportLink, TermsAndPrivacy
 # Register your models here.
 
-admin.site.register(UserManager)
 admin.site.register(User)
 admin.site.register(PatientProfile)
 admin.site.register(UserSettings)
