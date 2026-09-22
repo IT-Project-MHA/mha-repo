b6a883fe0b7563c61b357317a62decb8fd21e2ed
jfarinski-unimelb <josh.farinski@student.unimelb.edu.au>
2026-09-22 17:17:13 +1000

    Question component base outline

--- apps/mobile/components/baseComponents/question.tsx
+++ apps/mobile/components/baseComponents/question.tsx
@@ -1,3 +1,42 @@
 //what is the shared wrapper:
 //heading
 // instruction text
+import type React from "react";
+import { Text, View, StyleSheet } from "react-native";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+
+class QuestionProps {
+  title?: String;
+  detail?: String;
+}
+
+export default function Question ({
+  title,
+  detail}: QuestionProps) {
+
+  return (
+    <SafeAreaView>
+      <Text style={Styles.title}>
+        {title}
+      </Text>
+      // divider
+      <Text style={Styles.detail}>
+        {detail}
+      </Text>
+    </SafeAreaView>
+  );
+}
+
+export { Question };
+
+const Styles = StyleSheet.create({
+  title: {
+    margin: 10,
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  detail: {
+    margin: 8,
+    fontSize: 15,
+  }
+})
